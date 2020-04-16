3965930c1ffe363eed6bb38a5c82e01fa364375c
gdashua <gdashua@gmail.com>
2020-04-17 00:38:10 +0100

    Update findInterns.js

--- findInterns.js
+++ findInterns.js
@@ -5,7 +5,7 @@ MongoClient.connect(url,
 function(err, db){
 	if(err) throw err;
 	var dbObj = db.db("Gamey");
-
+//fetches the first documents from myMovie collection
 	dbObj.collection("myMovies").findOne({}, function(err, result){
 		if(err) throw err;
 		console.log("The first document in this collection is "+result.name);
