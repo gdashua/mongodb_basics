c421731f3f5a49784d1b8c5952839ae22ea1b372
gdashua <gdashua@gmail.com>
2020-04-17 00:21:57 +0100

    Update interns.js

--- interns.js
+++ interns.js
@@ -5,10 +5,10 @@ var url = "mongodb://localhost:27017";
 MongoClient.connect(url, function(err, db){
 if(err) throw err;
 var dbObj = db.db("Gamey");
-var myDocs = [{movie:"The Banker",year:"2020",rating:8}
-	  		  {movie:"Bad Boys",year:"2020",rating:7}
-	  		  {movie:"The Hunt",year:"2020",rating:7}
-	  		  {movie:"Bloodshot",year:"2020",rating:7.5}
+var myDocs = [{movie:"The Banker",year:"2020",rating:8},
+	  		  {movie:"Bad Boys",year:"2020",rating:7},
+	  		  {movie:"The Hunt",year:"2020",rating:7},
+	  		  {movie:"Bloodshot",year:"2020",rating:7.5},
 	  		  {movie:"First Cow",year:"2020",rating:6.5}
 	  		  
 ];
