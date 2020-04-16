7a4c64e3b691b6d266822560c168f9fcfb5c524c
gdashua <gdashua@gmail.com>
2020-04-17 00:45:11 +0100

    Update updateInterns.js

--- updateInterns.js
+++ updateInterns.js
@@ -4,12 +4,13 @@ var url = "mongodb://localhost:27017";
 MongoClient.connect(url, function(err, db){
 if(err) throw err;
 var dbObj = db.db("myMovies");
-var query = {movie: "The Banker" };
-var newMovie ={$set: {movie:"Money Heist S4", year:"2020", rating:9}};
+var query = {movie: "The Banker" };//this takes the program to the document with a movie value of 'The Banker'
+var newMovie ={$set: {movie:"Money Heist S4", year:"2020", rating:9}};//this species the document that will replace the document defined by the query
 
+  //updateOne option overides the queried document by replacing it with the newMovie document
 dbObj.collection("myMovies").updateOne(query,newMovie, function(err, res){
 if(err) throw err;
-console.log("1 document successfully inserted");
+console.log("1 document successfully inserted");// prints the result of operation
 db.close();
 });
 });
